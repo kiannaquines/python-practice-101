fc7f7f664282fc44d93b62f8207a3b4135798eac
kiannaquines <kjgnaquines@gmail.com>
2025-02-23 10:52:12 +0800

    added a edge case for only small letters

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,6 @@
 def palindrome(word:str) -> bool:
   length = len(word)
+  word = word.lower()
   for i in range(length // 2):
     if word[i] != word[length - i - 1]:
       return False
@@ -7,10 +8,8 @@ def palindrome(word:str) -> bool:
   return True
 
 
-print(palindrome("madam"))
-
-
-
+print(palindrome("madaM"))
 
 def palindrome(word:str) -> bool:
+  word = word.lower()
   return word == word[::-1]
