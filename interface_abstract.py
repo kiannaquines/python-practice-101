dce4681fce5d8109ca213d1bc479f8459d0a6c55
Kian Naquines <kjgnaquines@gmail.com>
2025-03-10 14:46:16 +0800

    Update interface_abstract.py

--- interface_abstract.py
+++ interface_abstract.py
@@ -22,12 +22,20 @@ class Ticket(ABC):
 
 class BusTicket(Ticket):
     def __init__(self, ticket_number, ticket_amount):
-        super().__init__(ticket_number, ticket_number)
+        super().__init__(ticket_number, ticket_amount)
         
     def get_ticket_information(self):
         ticket_information = f'Ticket No: {self.ticket_number} \nTicket Amount: {self.ticket_amount}'
         return ticket_information
 
+    def get_ticket_number(self):
+        return self.ticket_number
+        
+    def get_ticket_amount(self):
+        return self.ticket_amount
+    
 
 bus_ticket = BusTicket("1234567890", "1000")
 print(bus_ticket.get_ticket_information())
+print(bus_ticket.get_ticket_amount())
+print(bus_ticket.get_ticket_number())
