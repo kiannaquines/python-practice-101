4cc3496f44372ba9357ad25f47ecca0d29eb3372
Kian Naquines <kjgnaquines@gmail.com>
2025-03-16 20:54:10 +0800

    Update binary_search_tree.py
    added search and delete

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -33,6 +33,51 @@ class Tree:
             self._display(current.right, level + 1)
             print(" " * level + str(current.value))
             self._display(current.left, level + 1)
+            
+    def search(self, value):
+        return self._search(self.root, value)
+    
+    def _search(self, current, value):
+        if current is None or current.value == value:
+            return current
+        
+        if value < current.value:
+            return self._search(current.left, value)
+        elif value > current.value:
+            return self._search(current.right, value)
+
+    def _min_value_node(self, value):
+        current = value
+        while current.left is not None:
+            current = current.left
+        
+        return current
+    
+    def delete(self, value):
+        self.root = self._delete(self.root, value)
+    
+    def _delete(self, current, value):
+        if current is None:
+            return current
+        
+        if value < current.value:
+            current.left = self._delete(current.left, value)
+        elif value > current.value:
+            current.right = self._delete(current.right, value)
+        else:
+            
+            if current.left is None or current.right is None:
+                return None
+            
+            if current.left is not None:
+                return current.right
+            if current.right is not None:
+                return current.left
+            
+            temp = self._min_value_node(current.right)
+            current.value = temp.value
+            current.right = self._delete(current.right, temp.value)
+        return current
 
 if __name__ == "__main__":
     tree = Tree()
