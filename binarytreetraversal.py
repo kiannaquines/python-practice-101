ba7790289d41505297d94a2c4516c628ef66b158
Kian Naquines <kjgnaquines@gmail.com>
2025-04-17 19:58:26 +0800

    Update binarytreetraversal.py

--- binarytreetraversal.py
+++ binarytreetraversal.py
@@ -1,74 +1,63 @@
 class TreeNode(object):
     def __init__(self, value = 0, left = None, right = None):
         self.value = value
-        self.right = right
         self.left = left
-    
+        self.right = right
 
-class BinaryTreeTraversal(object):
-    
-    # left -> root -> right
-    def inorderTraversal(self, root):
-        
+class BinaryTraversal(object):
+    def inOrderTraversal(self, root):
         result = []
 
         def inorder(node):
             if node is None:
                 return
-
+            
             inorder(node.left)
             result.append(node.value)
             inorder(node.right)
         
         inorder(root)
-
         return result
 
-    # left -> right -> root
-    def postorderTraversal(self, root):
-
+    def postOrderTraversal(self, root):
         result = []
 
-        def postorder(node):
-            if node is None:
-                return
+        def postorder(root):
+            if root is None:
+                return 
             
-            postorder(node.left)
-            postorder(node.right)
-            result.append(node.value)
+            postorder(root.left)
+            postorder(root.right)
+            result.append(root.value)
         
         postorder(root)
-
         return result
-
-    # root -> left -> right
-    def preorderTraversal(self, root):
+    
+    def preOrderTraversal(self, root):
         result = []
-        def preorder(node):
-            if node is None:
-                return
+
+        def preorder(root):
+            if root is None:
+                return 
             
-            result.append(node.value)
-            preorder(node.left)
-            preorder(node.right)
+            result.append(root.value)
+            preorder(root.left)
+            preorder(root.right)
         
         preorder(root)
         return result
 
-
-
-
 if __name__ == "__main__":
-    root = TreeNode(4)
-    root.left = TreeNode(2)
-    root.right = TreeNode(5)
-    root.left.right = TreeNode(6)
+    root = TreeNode(2)
+    root.left = TreeNode(3)
+    root.right = TreeNode(4)
+    root.left.right = TreeNode(4)
 
-    sol = BinaryTreeTraversal()
-    resultInorder = sol.inorderTraversal(root)
-    resultPostorder = sol.postorderTraversal(root)
-    resultPreorder = sol.preorderTraversal(root)
-
-    print(resultInorder)
-    print(resultPostorder)
-    print(resultPreorder)
+    traversal = BinaryTraversal()
+    resultInorder = traversal.inOrderTraversal(root)
+    resultPostorder = traversal.preOrderTraversal(root)
+    resultPreorder = traversal.postOrderTraversal(root)
+    
+    print("IN-ORDER", resultInorder)
+    print("PRE-ORDER", resultPreorder)
+    print("POST-ORDER", resultPostorder)
