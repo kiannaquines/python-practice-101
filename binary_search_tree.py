7468704677d9ae35b1fdb617e27e7ec4bb6a668c
Kian Naquines <kjgnaquines@gmail.com>
2025-04-17 19:38:34 +0800

    Update binary_search_tree.py

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,10 +1,11 @@
-class Node:
-    def __init__(self, value):
+class TreeNode(object):
+    def __init__(self, value = 0, left = None, right = None):
         self.value = value
         self.left = None
         self.right = None
-        
-class BinarySearch:
+
+
+class BinarySearchTree:
     def __init__(self):
         self.root = None
     
@@ -13,7 +14,7 @@ class BinarySearch:
     
     def _insert(self, current, value):
         if current is None:
-            return Node(value)
+            return TreeNode(value)
         
         if value < current.value:
             current.left = self._insert(current.left, value)
@@ -21,88 +22,86 @@ class BinarySearch:
             current.right = self._insert(current.right, value)
         
         return current
-        
+    
     def display(self):
-        return self._display(self.root, 0)
+        self._display(self.root, 0)
     
     def _display(self, current, level):
         if current is not None:
             self._display(current.right, level + 1)
-            print(" " * 4 * level + str(current.value))
+            print(" " * 5 * level + str(current.value))
             self._display(current.left, level + 1)
-            
-            
-    def search(self, search_value):
-        return self._search(self.root, search_value)
-    
-    def _search(self, current, search_value):
-        if current is None or current.value == search_value:
-            return current
-        
-        if search_value < current.value:
-            return self._search(current.left, search_value)
-        elif search_value > current.value:
-            return self._search(current.right, search_value)
-        
+
+    def _min_value_node(self, right):
+        current = right
+
+        while current and current.right is not None:
+            current = current.right
         return current
-    
-    def _min_node_value(self, value):
-        current = value
-        
-        while current and current.left is not None:
-            current = current.left
-        
-        return current
-    
+
     def delete(self, value):
         self.root = self._delete(self.root, value)
     
     def _delete(self, current, value):
         if current is not None:
-            
+
             if value < current.value:
                 current.left = self._delete(current.left, value)
             elif value > current.value:
                 current.right = self._delete(current.right, value)
             else:
-                # case 1
+
                 if current.left is None:
                     return current.right
-                elif current.right is None:
-                    return current.left
                 
-                # case 2
-                temp = self._min_node_value(current.right)
+                if current.right is None:
+                    return current.left
+
+                # inorder swap
+                temp = self._min_value_node(current.right)
                 current.value = temp.value
                 current.right = self._delete(current.right, temp.value)
-            
-            return current                
-            
         
+        return current
+
+
+    def search(self, value):
+        return self._search(self.root, value)
     
-tree = BinarySearch()
-tree.insert(2)
-tree.insert(1)
-tree.insert(4)
-tree.insert(5)
-tree.insert(6)
-tree.insert(7)
+    def _search(self, current, value):
+        if current is not None:
 
-tree.display()
+            if current is None or current.value == value:
+                return current
+            
+            if value < current.value:
+                return self._search(current.left, value)
+            elif value > current.value:
+                return self._search(current.right, value)
+            
+if __name__ == "__main__":
+    tree = BinarySearchTree()
+    tree.insert(1)
+    tree.insert(-1)
+    tree.insert(3)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(6)
+    tree.insert(8)
+    tree.insert(9)
+    tree.insert(7)
 
-search_node = 10
+    tree.delete(7)
+    tree.delete(9)
+    tree.delete(4)
+    tree.delete(3)
+    tree.display()
 
 
-if tree.search(search_node) is not None:
-    print(f"Node {search_node} is found")
-else:
-    print(f"Node {search_node} cannot be found")
+    search = tree.search(-2)
 
-delete_node = 7    
-    
-if tree.delete(delete_node):
-    print(f"Node {delete_node} has been deleted")
-else:
-    print(f"Node {delete_node} cannot be deleted")
+    if search is not None:
+        print(f"Node {search.value} is found in the tree")
+    else:
+        print(f"Node is not found in the tree")
 
-tree.display()
