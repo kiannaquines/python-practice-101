ca7c9b17118b646c98ec4d4d195db70a4f52e72e
Kian Naquines <kjgnaquines@gmail.com>
2025-04-18 19:41:35 +0800

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,46 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, value):
+        return self.items.append(value)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+    
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError('Stack is empty')
+        
+        return self.items[-1]
+
+    def __str__(self):
+        return f"Stack {self.items}"
+
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+    stack.push(6)
+    stack.push(7)
+    stack.push(8)
+    stack.push(9)
+    stack.push(10)
+
+    print(stack)
+
+    size = stack.size()
+
+    print(size)
+
+    stack.pop()
+
+    print(stack)
