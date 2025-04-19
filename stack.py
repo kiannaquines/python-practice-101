425dc48b11986fbd3dd4889709b1b82bd06fcb2b
Kian Naquines <kjgnaquines@gmail.com>
2025-04-19 21:44:33 +0800

    Update stack.py

--- stack.py
+++ stack.py
@@ -6,54 +6,51 @@ class Stack:
         return self.items.append(value)
     
     def pop(self):
+        if self.is_empty():
+            raise IndexError('Cannot pop when stack is empty.')
+        
         return self.items.pop()
+    
+    def reverse(self):
+        if self.is_empty():
+            raise IndexError('Cannot reverse when stack is empty')
+
+        return self.items[::-1]
+    
+    def peek(self):
+        if self.is_empty():
+            raise IndexError('Stack is empty, you cannot peek it out.')
+        return self.items[-1]
     
     def size(self):
         return len(self.items)
     
     def is_empty(self):
-        return len(self.items) == 0
-
-    def peek(self):
-        if self.is_empty():
-            raise IndexError('Stack is empty')
-        
-        return self.items[-1]
-
-    def reverse(self):
-        self.items = self.items[::-1]
-
-        return self.items
+        return self.items == 0
 
     def clear(self):
         return self.items.clear()
-
+    
     def __str__(self):
-        return f"Stack {self.items}"
+        return f"Stack Items: {self.items}"
+
 
 if __name__ == "__main__":
     stack = Stack()
     stack.push(2)
     stack.push(3)
+    stack.push(3)
+    stack.push(2)
     stack.push(4)
-    stack.push(5)
-    stack.push(6)
     stack.push(7)
-    stack.push(8)
-    stack.push(9)
+    stack.push(3)
     stack.push(10)
 
-    print(stack)
-
-    size = stack.size()
-
-    print(size)
-
     stack.pop()
-
     print(stack)
-
     print(stack.reverse())
+    print(stack.peek())
+    print(stack.size())
 
     stack.clear()
 
