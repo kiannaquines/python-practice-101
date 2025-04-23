a4b0d13e686d985cf8ca6acd4cb91d877f2ed1e8
Kian Naquines <kjgnaquines@gmail.com>
2025-04-23 21:28:45 +0800

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,25 @@
-from random import randint, choice
-
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
+def binary_search(items, target):
+    left, right = 0, len(items) - 1
 
     while left <= right:
         middle = (left + right) // 2
 
-        if arr[middle] == target:
+        if items[middle] == target:
             return middle
-        elif arr[middle] > target:
+        elif items[middle] > target:
             right = middle - 1
         else:
             left = middle + 1
     
     return -1
 
-if __name__ == "__main__":
-    grades = [randint(75,100) for _ in range(50)]
-    grades = sorted(list(set(grades)))
-    search = binary_search(grades, choice(grades))
 
-    if search > -1:
-        print(f"Item found in {search}")
-    else:
-        print(f"Item cannot be found")
+if __name__ == "__main__":
+
+    from random import randint, choice
+
+    grades = [randint(75,100) for _ in range(50)]
+    grades = list(set(sorted(grades)))
+
+    search = binary_search(grades, choice(grades))
+    print(search)
