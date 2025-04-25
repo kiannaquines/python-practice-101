e2a0a1b3666a82062ce8feb187c1b7d5f9398921
Kian Naquines <kjgnaquines@gmail.com>
2025-04-25 09:31:35 +0800

    Update isEven.py

--- isEven.py
+++ isEven.py
@@ -1,8 +1,17 @@
-def isEven(number):
-    if number % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
+def isEven(n):
+    if n == 0:
+        return True
 
+    if n % 2 == 0:
+        return True
+    
+    return False
 
-print(isEven(3))
+import random
+
+numbers = [random.randint(0, 100) for _ in range(100)]
+
+for number in numbers:
+
+    isEvenResult = isEven(number)
+    print(f'{number} {isEvenResult}')
